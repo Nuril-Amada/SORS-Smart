b81f10ea4065a60d06d137b297835682b5ff595a
Nuril-Amada <amadanuril@gmail.com>
2026-09-22 10:45:01 +0700

    Revisi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
 import Login from './pages/login';
 import Navbar from './pages/Navbar';
